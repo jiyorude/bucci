3abc105b804b5ed2433583690541cba42a0d18bb
Jordy Veenstra <jordy.gaptx@gmail.com>
2024-12-18 13:49:51 +0100

    18/12 13:49
    sql update

--- src/database/bucci.sql
+++ src/database/bucci.sql
@@ -1,1 +1,13 @@
 CREATE DATABASE IF NOT EXISTS bucci;
+
+CREATE TABLE cms (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    short_description TEXT NOT NULL, 
+    intro TEXT NOT NULL,
+    image_link VARCHAR(2083),
+    publish_date DATE NOT NULL,
+    mins_read INT, 
+    author VARCHAR(100) NOT NULL,
+    main_content TEXT NOT NULL
+);
